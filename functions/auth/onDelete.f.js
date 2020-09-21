7794c679a3a66db9c5e8fca2ace64df753e16ba6
Akoji Francis <jidwithfa007@crmintcol.org>
2020-09-21 10:39:14 +0100

    Updated Functions

--- functions/auth/onDelete.f.js
+++ functions/auth/onDelete.f.js
@@ -4,74 +4,73 @@ import moment from 'moment'
 import nodemailer from 'nodemailer'
 
 const gmailEmail = encodeURIComponent(
-  functions.config().gmail ? functions.config().gmail.email : ''
+    functions.config().gmail ? functions.config().gmail.email : ''
 )
 const gmailPassword = encodeURIComponent(
-  functions.config().gmail ? functions.config().gmail.password : ''
+    functions.config().gmail ? functions.config().gmail.password : ''
 )
 const mailTransport = nodemailer.createTransport(
-  `smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com`
+    `smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com`
 )
 
 export default functions.auth.user().onDelete((userMetadata, context) => {
-  const uid = userMetadata.uid
-  const email = userMetadata.email
-  const displayName = userMetadata.displayName
+    const uid = userMetadata.uid
+    const email = userMetadata.email
+    const displayName = userMetadata.displayName
 
-  console.log(userMetadata.providerData)
-  console.log(userMetadata)
-  console.log(context)
+    console.log(userMetadata.providerData)
+    console.log(userMetadata)
+    console.log(context)
 
-  const provider = userMetadata.providerData.length
-    ? userMetadata.providerData[0]
-    : { providerId: email ? 'password' : 'phone' }
-  const providerId = provider.providerId
-    ? provider.providerId.replace('.com', '')
-    : provider.providerId
+    const provider = userMetadata.providerData.length ?
+        userMetadata.providerData[0] : { providerId: email ? 'password' : 'phone' }
+    const providerId = provider.providerId ?
+        provider.providerId.replace('.com', '') :
+        provider.providerId
 
-  let promises = []
+    let promises = []
 
-  const mailOptions = {
-    from: `"Tarik Huber" <${gmailEmail}>`,
-    to: email,
-    subject: `Bye!`,
-    text: `Hey ${displayName ||
-      ''}!, We confirm that we have deleted your React Most Wanted account.`,
-  }
+    const mailOptions = {
+        from: `"Akoji Francis" <${gmailEmail}>`,
+        to: email,
+        subject: `Bye!`,
+        text: `Hey ${displayName ||
+      ''}!, We confirm that we have deleted your Versabit Task account.`,
+    }
 
-  const sendEmail = mailTransport.sendMail(mailOptions).then(() => {
-    console.log('Account deletion confirmation email sent to:', email)
-    return null
-  })
+    const sendEmail = mailTransport.sendMail(mailOptions).then(() => {
+        console.log('Account deletion confirmation email sent to:', email)
+        return null
+    })
 
-  const deleteUser = admin
-    .database()
-    .ref(`/users/${uid}`)
-    .set(null)
-  const deleteTokens = admin
-    .database()
-    .ref(`/notification_tokens/${uid}`)
-    .set(null)
-  const deleteChats = admin
-    .database()
-    .ref(`/users_chats/${uid}`)
-    .set(null)
-
-  const usersCount = admin
-    .database()
-    .ref(`/users_count`)
-    .transaction(current => (current || 0) - 1)
-
-  if (providerId) {
-    promises.push(
-      admin
+    const deleteUser = admin
         .database()
-        .ref(`/provider_count/${providerId}`)
+        .ref(`/users/${uid}`)
+        .set(null)
+    const deleteTokens = admin
+        .database()
+        .ref(`/notification_tokens/${uid}`)
+        .set(null)
+    const deleteChats = admin
+        .database()
+        .ref(`/users_chats/${uid}`)
+        .set(null)
+
+    const usersCount = admin
+        .database()
+        .ref(`/users_count`)
         .transaction(current => (current || 0) - 1)
-    )
-  }
 
-  promises.push(sendEmail, deleteUser, usersCount, deleteTokens, deleteChats)
+    if (providerId) {
+        promises.push(
+            admin
+            .database()
+            .ref(`/provider_count/${providerId}`)
+            .transaction(current => (current || 0) - 1)
+        )
+    }
 
-  return Promise.all(promises)
+    promises.push(sendEmail, deleteUser, usersCount, deleteTokens, deleteChats)
+
+    return Promise.all(promises)
 })
